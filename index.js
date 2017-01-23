46f630d3a0bbab0de3860a62bd939245efe5a773
Rob Hitt <robhitt@gmail.com>
2017-01-22 21:36:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {addBump, getBumps}
+}
+
+function createAnimal(animalType) {
+  return  (deadlyDevice) => {
+    return { animalType, deadlyDevice }
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
